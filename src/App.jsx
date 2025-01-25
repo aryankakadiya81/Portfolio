86529c87e2be0b8633589610c5aadf5f97479c24
aryankakadiya81 <aryankakadiya81@gmail.com>
2025-01-25 19:51:50 +0530

    All Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-// import Navbar from './Components/Navbar'
-import Hero_Section from './Components/Hero_Section'
-import Team_Section from './Components/Team_Section'
-import Contect_Section from './Components/Contect_Section'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Combine from './Components/Combine'
+import React from 'react';
+import { use } from 'react';
+import { useState } from 'react';
+
 
 const App = () => {
+  let [Amt, SetAmt] = useState(0);
+  let [Gstper, SetGstper] = useState(5);
+  let [Profitper, SetProfitper] = useState(30);
+
+  let Profit = (Profitper * Amt) / 100;
+  let Gst = ((Gstper * (Amt + Profit)) / 100);
+  let Total = Amt + Profit + Gst;
   return (
     <>
-      <BrowserRouter>
-        <Navbar></Navbar>
-
-        <Routes>
-          <Route path='/' element={<Combine></Combine>}></Route>
-          <Route path='/Team' element={<Team_Section></Team_Section>}></Route>
-          <Route path='/Contect' element={<Contect_Section></Contect_Section>}></Route>
-          <Route path='/About' element={<Hero_Section></Hero_Section>}></Route>
-
-
-
-        </Routes>
-
-        <Footer></Footer>
-      </BrowserRouter>
+      <div style={{fontSize:"20px",textAlign:"center"}}>
+        <div>
+          Ammount:  
+          <input
+          style={{fontSize:"20px"}}
+            type="text"
+            value={Amt}
+            onChange={(e) => SetAmt(+e.target.value)}
+          />
+        </div>
+        <div>Profit{`(${Profitper}%)`}:<div>{Profit}</div></div>
+        <div>Gst{`(${Gstper}%)`}</div><div>{Gst}</div>
+        <div>Total value</div><div>{Total}</div>
+      </div>
     </>
   )
 }
 
 export default App
-// <Navbar></Navbar>
